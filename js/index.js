4225ad1d682ada47ceb8e9f1ce582f2c7f4b3b9d
alphite219 <alfiewright219@gmail.com>
2023-05-03 19:15:15 +0100

    Created more project entries

--- js/index.js
+++ js/index.js
@@ -1,33 +1,36 @@
 const doodle = document.getElementsByClassName("doodles")[0]
 const astral = document.getElementById("astral")
 const foundation = document.getElementById("foundation")
+const gong = document.getElementById("gong")
 
 let currentDoodle
 let rotateInterval
 let rotation = 0
+let isRotating = false
 
 astral.onmouseover = function() {changeDoodleImage("astral","images/astral-doodle.gif",1)}
-foundation.onmouseover = function() {changeDoodleImage("foundation", "images/drawing-doodle.gif",0)}
+foundation.onmouseover = function() {changeDoodleImage("foundation", "images/foundation-doodle.gif",0)}
+gong.onmouseover = function() {changeDoodleImage("gong", "images/drawing-doodle.gif",0)}
 
 function changeDoodleImage(p1, p2, p3){
     if(currentDoodle != p1){
+        if(p3 == 1){
+            startRotate()
+        }else{
+            stopRotate()
+            //console.log(p1,p2,p3)
+        }
         doodle.style.opacity = 0
         setTimeout(() => {
             currentDoodle = p1
             doodle.src = p2
             doodle.style.opacity = 1
-        }, 500);
-
-        if(p3 == 1){
-            startRotate()
-        }else{
-            stopRotate()
-            console.log(p1,p2,p3)
-        }
+        }, 400);
     }
 }
 
 function startRotate(){
+    isRotating = true
     rotateInterval = setInterval(function(){
         rotate()
     }, 50);
@@ -36,13 +39,15 @@ function startRotate(){
 
 function stopRotate(){
     clearInterval(rotateInterval);
+    isRotating = false
     console.log("stopped")
     doodle.style.transform = "rotate(0deg)"
     rotation = 0
 }
 
 function rotate(){
-    rotation += 0.25
-    console.log(rotation)
-    doodle.style.transform = "rotate(" + rotation + "deg)"
+    if(isRotating==true){
+        rotation += 0.25
+        doodle.style.transform = "rotate(" + rotation + "deg)"
+    }
 }
